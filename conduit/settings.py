7966997d1b4f6389e9e09b833722189fb7189377
RawatTushar <tusharrawatdpss@gmail.com>
2026-05-16 12:02:24 +0530

    ....migration completed to Mysql from sqlite and  containerize my application using  docker and deployed via kubernetes (minikube)

--- conduit/settings.py
+++ conduit/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'db',
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME', 'django_db'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
